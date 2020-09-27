8b3288c7748a46a968160f49a58d43f5e3ffa12c
mmead <mmeadx@gmail.com>
2020-09-27 18:33:37 -0500

    data shows in console

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,12 @@
-d3.json("/samples.json", function(data) {
-    console.log(data);
+// console.log("Test");
+
+d3.json("/samples.json").then(function(data) {
+
+    // Grab values from the response json object to build the plots
+    var name = data.names;
+    console.log(name);
+    var metadata = data.metadata;
+    console.log(metadata);
+    var samples = data.samples;
+    console.log(samples);
 });
