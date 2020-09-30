e587fb425b003d4f24b53c4fb781846c636baa6f
mmead <mmeadx@gmail.com>
2020-09-30 10:02:59 -0500

    Variables for buildPlots function attained - need to build plots

--- static/js/app.js
+++ static/js/app.js
@@ -17,6 +17,7 @@ function updateAll() {
 
     var dataset = dropdownMenu.node().value;
 
+    console.log("***---***---***"); // Will run when new dropdown item is selected
     console.log("*** updateAll Function Running ***"); // Tell console the function is running
     buildDemo(dataset);
     buildPlots(dataset);
@@ -30,9 +31,11 @@ function buildDemo(info){
     d3.json("/samples.json").then((meta) => {
 
         console.log("*** buildDemo function running ***"); // Tell console the function is running
-        console.log(`buildDemo : ${info}`);
-
+        
+        // Grab selected Dataset from Metadata using Subject ID no. from dropdown
         var selectedData = meta.metadata.filter(x => x.id === parseInt(info));
+        
+        console.log(`buildDemo using ${info}`);
         console.log(selectedData[0]);
 
         // Use D3 to select Demographic Panel to put sample info
@@ -48,6 +51,20 @@ function buildDemo(info){
     };
 
 function buildPlots(info){
+    d3.json("/samples.json").then((meta) => {
+
+        console.log("*** buildPlots function running ***");
+        console.log(`buildPlots on ${info}`);
+
+        // Grab selected Dataset from samples using Subject ID No. from dropdown
+        var selectedData = meta.samples.filter(x => x.id === info);
+
+        var sample_values = selectedData[0].sample_values;
+        var otu_ids = selectedData[0].otu_ids;
+        var otu_labels = selectedData[0].otu_labels
+
+        // console.log(otu_labels); // TEST
+    })
     // console.log("buildPlots Function Running");
     // console.log(`sample data of ${info}`);
     
